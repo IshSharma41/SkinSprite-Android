5f9fe6e57a69dc552e7386c086e43dd8ef272ba3
IshSharma41 <ishsharma41@gmail.com>
2024-04-06 01:19:19 +0530

    Adding the response model. make sure to change the baseurl to the current one if the model is not automated hosted.

--- app/src/main/java/com/example/project_techmind_android/UploadActivity.java
+++ app/src/main/java/com/example/project_techmind_android/UploadActivity.java
@@ -1,127 +1,180 @@
 package com.example.project_techmind_android;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
-import android.view.View;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
-import androidx.annotation.NonNull;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
+
+import com.google.gson.JsonObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 
-public class UploadActivity extends AppCompatActivity implements SurfaceHolder.Callback {
-        private static final int UPLOAD_RESULT_CODE = 101;
+import okhttp3.MediaType;
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
-        // Your activity code...
+public class UploadActivity extends AppCompatActivity {
 
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private static final String TAG = "MainActivity";
 
-    private static final int REQUEST_IMAGE_FROM_GALLERY = 2;
-    private static final int REQUEST_CAMERA_PERMISSION = 101;
-    private SurfaceView cameraPreview;
-    private SurfaceHolder surfaceHolder;
-    private CameraHelper cameraHelper;
+    private ImageView imageView;
+    private Button uploadButton;
+    private ApiService apiService;
+    private Uri imageUri;
+
+    private TextView resultTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
-        cameraPreview = findViewById(R.id.camera_preview);
-        cameraPreview.setVisibility(View.VISIBLE);
-        cameraPreview.setZOrderMediaOverlay(true);
-        surfaceHolder = cameraPreview.getHolder();
-        surfaceHolder.addCallback(this);
 
-        ImageView imageView = findViewById(R.id.imageView);
+        imageView = findViewById(R.id.imageView);
+        uploadButton = findViewById(R.id.uploadButton);
+        resultTextView = findViewById(R.id.resultTextView);
 
-        Button btnGallery = findViewById(R.id.btnGallery);
-        btnGallery.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openGallery();
-            }
-        });
+        Retrofit retrofit = new Retrofit.Builder()
+                                    .baseUrl("https://85da-14-139-241-214.ngrok-free.app")
+                                    .addConverterFactory(GsonConverterFactory.create())
+                                    .build();
+        apiService = retrofit.create(ApiService.class);
 
-        Button btnCapture = findViewById(R.id.btnCapture);
-        btnCapture.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (cameraHelper != null) {
-                    cameraHelper.takePicture();
-                }
-            }
-        });
+        uploadButton.setOnClickListener(v -> openGallery());
     }
 
     private void openGallery() {
         Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        startActivityForResult(galleryIntent, REQUEST_IMAGE_FROM_GALLERY);
+        startActivityForResult(galleryIntent, PICK_IMAGE_REQUEST);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
-        if (resultCode == RESULT_OK && requestCode == REQUEST_IMAGE_FROM_GALLERY && data != null) {
-            // Code for handling gallery image selection
-        } else if (resultCode == RESULT_OK && requestCode == CameraHelper.REQUEST_IMAGE_CAPTURE) {
-            // Code for handling camera image capture
-        }
-
-// Receive JSON string from UploadImageTask
-        if (resultCode == RESULT_OK && requestCode == UPLOAD_RESULT_CODE && data != null) {
-            String jsonString = data.getStringExtra("jsonString");
-            // Start DisplayResultActivity and pass JSON string as extra
-            Intent intent = new Intent(UploadActivity.this, DisplayResultActivity.class);
-            intent.putExtra("jsonString", jsonString);
-            startActivity(intent);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null) {
+            imageUri = data.getData();
+            imageView.setImageURI(imageUri);
+            uploadImage();
         }
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == REQUEST_CAMERA_PERMISSION) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                if (surfaceHolder != null) {
-                    cameraHelper = new CameraHelper(UploadActivity.this, surfaceHolder, findViewById(R.id.imageView));
+    private void uploadImage() {
+        if (imageUri != null) {
+            try {
+                // Read image file into byte array
+                File imageFile = new File(getRealPathFromURI(imageUri));
+                FileInputStream fis = new FileInputStream(imageFile);
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                byte[] buffer = new byte[1024];
+                int read;
+                while ((read = fis.read(buffer)) != -1) {
+                    baos.write(buffer, 0, read);
                 }
-                cameraHelper.openCamera();
-            } else {
-                Toast.makeText(this, "Camera permission is required", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
+                fis.close();
+                byte[] imageData = baos.toByteArray();
 
-    @Override
-    public void surfaceCreated(@NonNull SurfaceHolder holder) {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
+                // Convert byte array to base64 string
+                String imageDataBase64 = android.util.Base64.encodeToString(imageData, android.util.Base64.DEFAULT);
+
+                // Create JSON object with image data
+                JsonObject jsonObject = new JsonObject();
+                jsonObject.addProperty("image", imageDataBase64);
+
+                // Create request body with JSON content type
+                RequestBody requestBody = RequestBody.create(MediaType.parse("application/json"), jsonObject.toString());
+
+                // Proceed with Retrofit call
+                Call<JsonObject> call = apiService.uploadImage(requestBody);
+                call.enqueue(new Callback<JsonObject>() {
+                    @Override
+                    public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+                        if (response.isSuccessful()) {
+                            try {
+                                JsonObject responseBody = response.body();
+                                if (responseBody != null && responseBody.has("prediction")) {
+                                    // Extract the prediction JSON object
+                                    JsonObject predictionObject = responseBody.getAsJsonObject("prediction");
+
+                                    // Convert the prediction JSON object to a string for display
+                                    String predictionString = predictionObject.toString();
+
+                                    // Format the prediction string
+                                    StringBuilder formattedPrediction = new StringBuilder();
+                                    boolean isFirst = true;
+                                    for (int i = 0; i < predictionString.length(); i++) {
+                                        char c = predictionString.charAt(i);
+                                        if (c == ',') {
+                                            formattedPrediction.append(",\n");
+                                            isFirst = true;
+                                        } else {
+                                            if (isFirst) {
+                                                formattedPrediction.append("\n");
+                                                isFirst = false;
+                                            }
+                                            formattedPrediction.append(c);
+                                        }
+                                    }
+
+                                    // Set the formatted prediction string in the TextView
+                                    resultTextView.setText(formattedPrediction.toString());
+                                } else {
+                                    Log.e(TAG, "Prediction key not found in the response");
+                                    Toast.makeText(UploadActivity.this, "Prediction key not found in the response", Toast.LENGTH_SHORT).show();
+                                }
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                                Toast.makeText(UploadActivity.this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            Log.e(TAG, "Failed to receive prediction: " + response.message());
+                            Toast.makeText(UploadActivity.this, "Failed to receive prediction", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+
+                    @Override
+                    public void onFailure(Call<JsonObject> call, Throwable t) {
+                        // Handle failure here
+                        Log.e(TAG, "Network error: " + t.getMessage());
+                        Toast.makeText(UploadActivity.this, "Network error: " + t.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            } catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(UploadActivity.this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
         } else {
-            if (surfaceHolder != null) {
-                cameraHelper = new CameraHelper(UploadActivity.this, surfaceHolder, findViewById(R.id.imageView));
-            }
-            cameraHelper.openCamera();
+            Toast.makeText(this, "No image selected", Toast.LENGTH_SHORT).show();
         }
     }
 
-    @Override
-    public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
-        cameraHelper.startPreview();
-    }
-
-    @Override
-    public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
-        cameraHelper.releaseCamera();
+    private String getRealPathFromURI(Uri contentUri) {
+        String[] projection = { MediaStore.Images.Media.DATA };
+        Cursor cursor = getContentResolver().query(contentUri, projection, null, null, null);
+        if (cursor == null) {
+            // Error handling, return null or appropriate fallback
+            return null;
+        }
+        int columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        String filePath = cursor.getString(columnIndex);
+        cursor.close();
+        return filePath;
     }
 }
